d543f845dbcc05a4cba9f15e0d700ce5c9d281ac
ian.hersom <ian.hersom@gmail.com>
2025-03-23 16:30:14 -0600

    refactor main plugin lua script

--- lua/telescope-remote-buffer.lua
+++ lua/telescope-remote-buffer.lua
@@ -1,6 +1,13 @@
 local M = {}
 
 function M.setup(opts)
+    local default_mappings = {
+        fzf = "<leader>fz",
+        match = "<leader>gb",
+        oldfiles = "<leader>rb"
+    }
+    opts = opts or default_mappings -- Ensure opts exists otherwise set defaults
+
     local nmap = function(keys, func, desc)
         vim.keymap.set(
             'n',
@@ -17,29 +24,9 @@ function M.setup(opts)
     local multi_buffer_match = require("telescope_multi_buffer_match")
     local recent_buffers = require("telescope_recent_buffers")
 
-    local fzf_mapping
-    if opts.fzf then
-        fzf_mapping = opts.fzf
-    else
-        fzf_mapping = "<leader>fz"
-    end
-    nmap(fzf_mapping, function() multi_buffer_fzf.multi_buffer_fuzzy_find() end, "Fuzzy find in open buffers")
-
-    local match_mapping
-    if opts.match then
-        match_mapping = opts.match
-    else
-        match_mapping = "<leader>gb"
-    end
-    nmap(match_mapping, function() multi_buffer_match.multi_buffer_exact_find() end, "Grep through all open buffers for exact query match")
-
-    local oldfiles_mapping
-    if opts.oldfiles then
-        oldfiles_mapping = opts.oldfiles
-    else
-        oldfiles_mapping = "<leader>rb"
-    end
-    nmap(oldfiles_mapping, function() recent_buffers.show_recent_buffers() end, 'Show recently accessed buffers of all kinds')
+    nmap(opts.fzf, function() multi_buffer_fzf.multi_buffer_fuzzy_find() end, "Fuzzy find in open buffers")
+    nmap(opts.match, function() multi_buffer_match.multi_buffer_exact_find() end, "Grep through all open buffers for exact query match")
+    nmap(opts.oldfiles, function() recent_buffers.show_recent_buffers() end, 'Show recently accessed buffers of all kinds')
 end
 
 return M
